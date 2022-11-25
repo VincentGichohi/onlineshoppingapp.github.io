2395af4d3664e525d8976467b2be03a9e3c3b644
VincentGichohi <vincentegichohi@gmail.com>
2022-11-25 11:05:46 +0300

    add apps to installed apps for project

--- Online_Shop/settings.py
+++ Online_Shop/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cart.apps.CartConfig',
-
-    # apps
+    'orders.apps.OrdersConfig',
+    'payment.apps.PaymentConfig',
+    'coupons.apps.CouponsConfig',
 ]
 
 MIDDLEWARE = [
