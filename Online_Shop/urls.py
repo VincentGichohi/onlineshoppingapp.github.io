5c57d56aaf2c1e5873664335c5bd2057e6571a9d
VincentGichohi <vincentegichohi@gmail.com>
2022-11-25 11:04:35 +0300

    add app paths to project urls file

--- Online_Shop/urls.py
+++ Online_Shop/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
+    path('payment/', include(('payment.urls', 'payment'), namespace='payment')),
+    path('coupons/', include(('coupons.urls', 'coupons'), namespace='coupons')),
+    path('', include(('shop.urls', 'shop'), namespace='shop')),
+    path('orders/', include(('orders.urls', 'orders'), namespace='orders'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
