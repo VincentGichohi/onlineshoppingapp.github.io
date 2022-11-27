0d512159ac42bf6b1ab99a797029262139379643
VincentGichohi <vincentegichohi@gmail.com>
2022-11-27 22:31:55 +0300

    edit order method

--- orders/tasks.py
+++ orders/tasks.py
@@ -12,7 +12,7 @@ def order_created(order_id):
     Task to send an email-notification when an order is successfully created.
     """
     order = Order.objects.get(id=order_id)
-    subject = f"Order nr. {order.id}"
+    subject = f"Order no. {order.id}"
     message = f"Dear {order.first_name}, \n\n" \
               f"you have successfully placed an order." \
               f"Your order ID is {order.id}."
